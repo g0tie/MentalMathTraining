4226bb83b309e49783d471d1f69168f06a47b0ff
g0tie <gaultierbegue@outlook.com>
2021-08-08 14:25:13 +0400

    adding generation of random calculation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import RandomCalculation from './components/RandomCalculation';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {/* <Text>Open up App.tsx to start working on your app!</Text> */}
       <StatusBar style="auto" />
+      <RandomCalculation digitNumbers="2"/>
     </View>
   );
 }
---
+++ components/RandomCalculation.tsx
@@ -0,0 +1,45 @@
+import React , { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function RandomCalculation({digitNumbers}: any)
+{   
+  
+    const [calculation, setCalulation] = useState(generateRandomCalculation(digitNumbers));
+    
+    return (
+        <View style={calculationStyle.container}>
+            <Text style={calculationStyle.placeholder}> { calculation } </Text>
+        </View>
+    )
+}
+
+function generateRandomCalculation(digitNumbers: number): string
+{
+    let limit: string = "";
+    let operationTypes: string[] = ["+", "x", "÷", "-"];
+    
+    for (let i = 0; i < digitNumbers; i++) {
+        limit += '9';
+    }
+
+    let firstNumber: number = generateRandomNb(parseInt(limit));
+    let secondNumber: number = generateRandomNb(parseInt(limit));
+    let operationType: string = operationTypes[generateRandomNb(operationTypes.length)]
+
+
+    return `${firstNumber} ${operationType} ${secondNumber}`;
+}
+
+function generateRandomNb(limit: number): number
+{
+    return Math.floor(Math.random() * limit);
+}
+
+let calculationStyle = StyleSheet.create({
+    container: {
+
+    },
+    placeholder: {
+        fontSize: 'xxx-large'
+    }
+});
